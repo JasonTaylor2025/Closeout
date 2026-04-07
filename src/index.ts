8abb0b529693322c7197578a2f18ab76cd2fd23d
JasonTaylor2025 <jason.taylor@bekbg.com>
2026-04-07 10:38:33 -0400

    Delete src directory

--- src/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as CloseoutMatrixWebPart } from "./webparts/closeoutMatrix/CloseoutMatrixWebPart";
-
--- src/webparts/closeoutMatrix/components/CloseoutMatrix.module.scss.d.ts
+++
@@ -1,6 +0,0 @@
-declare const styles: {
-  readonly closeoutMatrixRoot: string;
-};
-
-export default styles;
-
